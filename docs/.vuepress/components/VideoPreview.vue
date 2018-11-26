6c48ea78b581e3fa8ebefd318eadc394dc145f8c
Jinjiang <zhaojinjiang@me.com>
2018-11-26 18:13:49 +0800

    docs: fixed autoplay issue in Chrome

--- docs/.vuepress/components/VideoPreview.vue
+++ docs/.vuepress/components/VideoPreview.vue
@@ -1,13 +1,7 @@
 <template>
-  <a v-if="!opened" href="#" @click.prevent="opened = true">
+  <a href="#" @click.prevent="preview">
     <img :src="img" :alt="title" style="border-radius: 6px;">
   </a>
-  <div v-else>
-    <iframe
-      width="640" height="360" frameborder="0" :src="url"
-      webkitallowfullscreen mozallowfullscreen allowfullscreen
-    ></iframe>
-  </div>
 </template>
 
 <script>
@@ -26,8 +20,26 @@ export default {
       default: "https://player.vimeo.com/video/297515936?autoplay=1"
     }
   },
-  data() {
-    return { opened: false };
+  methods: {
+    preview() {
+      // dropped v-if/v-else syntax to fix the autoplay issue in Chrome
+      // https://github.com/vuejs/vuex/pull/1453#issuecomment-441507095
+      const { $el, url } = this;
+      const video = document.createElement('iframe');
+      const attrs = {
+        width: 640,
+        height: 360,
+        frameborder: 0,
+        src: url,
+        webkitallowfullscreen: true,
+        mozallowfullscreen: true,
+        allowfullscreen: true
+      };
+      for (const name in attrs) {
+        video.setAttribute(name, attrs[name])
+      }
+      $el.parentNode.replaceChild(video, $el);
+    }
   }
 }
 </script>
